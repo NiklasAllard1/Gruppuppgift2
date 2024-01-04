667d46cc066bb489fe50eaeaa47b8e7b368efd08
alleh <alle.henriksson@hotmail.se>
2024-01-04 11:33:29 +0100

    first act

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public static class Division
+{
+    public static double Divide(double dividend, double divisor)
+    {
+        if (divisor != 0)
+        {
+            return dividend / divisor;
+        }
+        else 
+        {
+            throw new ArgumentException("Can't divide by 0");
+        }
+
+    }
+}
